87debdfb0dab385af874cfdd692eb605f37e14cf
Nelo07o <90777157+Nelo07o@users.noreply.github.com>
2023-05-26 21:11:35 +0200

    fix bouton et JavaScript

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -1,3 +1,5 @@
+import {mascotteLG, mascotteMD} from "./svg.js";
+
 export function handleCustomCursor(e) {
     const customCursor = document.querySelector(".custom-cursor");
     customCursor.style.transform = `translate(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%))`;
@@ -22,4 +24,56 @@ export function handleScroll() {
     } else {
         header.classList.remove("sticky");
     }
+}
+
+export function toggleToVisible() {
+
+    const elements = [
+        document.querySelector(".container-logo"),
+        document.querySelector(".container-nav"),
+        document.querySelector(".socials"),
+    ]
+
+    elements.forEach((element) => {
+        element.style.visibility = element.style.visibility === "visible" ? "hidden" : "visible";
+    });
+}
+
+export function toggleMenu() {
+    const header = document.querySelector(".header");
+    const hamburgerMenu = document.querySelector(".header__nav-toggle");
+    const page = document.documentElement;
+
+    hamburgerMenu.classList.toggle("menu--open");
+    header.classList.toggle("display-menu");
+    page.classList.toggle("no-scroll");
+    toggleToVisible();
+}
+
+export function setupMascotteContainers() {
+    const hero = document.querySelector(".hero");
+
+    const containerMascotteLG = document.createElement("div");
+    containerMascotteLG.className = "container-mascotte-lg";
+    containerMascotteLG.innerHTML = mascotteLG;
+    hero.appendChild(containerMascotteLG);
+
+    const containerMascotteMD = document.createElement("div");
+    containerMascotteMD.className = "container-mascotte-md";
+    containerMascotteMD.innerHTML = mascotteMD;
+    hero.appendChild(containerMascotteMD);
+
+    function toggleMascotteContainer() {
+        if (window.innerWidth > 2000) {
+            containerMascotteLG.style.display = "block";
+            containerMascotteMD.style.display = "none";
+        } else {
+            containerMascotteLG.style.display = "none";
+            containerMascotteMD.style.display = "block";
+        }
+    }
+
+    toggleMascotteContainer();
+
+    window.addEventListener("resize", toggleMascotteContainer);
 }
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,13 @@
 // Import.
-import {handleCustomCursor, slideDown, handleScroll} from "./functions.js";
-import {mascotteMD, mascotteLG} from "./svg.js";
+import {
+    handleCustomCursor,
+    slideDown,
+    handleScroll,
+    toggleMenu,
+    setupMascotteContainers
+} from "./functions.js";
 
-// Custom cursor.
+// Custom cursor
 window.addEventListener("mousemove", handleCustomCursor);
 
 // Smooth slide sur la section suivante.
@@ -12,54 +17,9 @@ heroPushLink.addEventListener("click", slideDown);
 // Rend sticky le header au scroll à partir de la seconde section.
 window.addEventListener("scroll", handleScroll);
 
-const header = document.querySelector(".header");
+// Le menue Hamburger
 const hamburgerMenu = document.querySelector(".header__nav-toggle");
-const page = document.documentElement;
+hamburgerMenu.addEventListener("click", toggleMenu);
 
-hamburgerMenu.addEventListener("click", () => {
-
-    hamburgerMenu.classList.toggle("menu--open");
-    header.classList.toggle("display-menu");
-    page.classList.toggle("no-scroll");
-    toggleToVisible();
-
-});
-
-function toggleToVisible() {
-
-    const elements = [
-        document.querySelector(".container-logo"),
-        document.querySelector(".container-nav"),
-        document.querySelector(".socials"),
-    ]
-
-    elements.forEach((element) => {
-        element.style.visibility = element.style.visibility === "visible" ? "hidden" : "visible";
-    });
-}
-
-const hero = document.querySelector(".hero");
-
-const containerMascotteLG = document.createElement("div");
-containerMascotteLG.className = "container-mascotte-lg";
-containerMascotteLG.innerHTML = mascotteLG;
-hero.appendChild(containerMascotteLG);
-
-const containerMascotteMD = document.createElement("div");
-containerMascotteMD.className = "container-mascotte-md";
-containerMascotteMD.innerHTML = mascotteMD;
-hero.appendChild(containerMascotteMD);
-
-function toggleMascotteContainer() {
-    if (window.innerWidth > 2000) {
-        containerMascotteLG.style.display = "block";
-        containerMascotteMD.style.display = "none";
-    } else {
-        containerMascotteLG.style.display = "none";
-        containerMascotteMD.style.display = "block";
-    }
-}
-
-toggleMascotteContainer();
-
-window.addEventListener("resize", toggleMascotteContainer);
+// Permet de switch de taille de mascotte pour le responsive
+setupMascotteContainers();
